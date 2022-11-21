8b93c602e033b1e7d702f0712ea130e78b76f73d
Olga Parfenova <work.parfenova@gmail.com>
2022-11-21 17:34:07 +0300

    The second task is solved

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let div = document.createElement('div');
+// Task 1:
+// let div = document.createElement('div');
+//
+// div.style.overflowY = 'scroll';
+// div.style.width = '50px';
+// div.style.height = '50px';
+// div.style.background = 'yellow';
+//
+// document.body.append(div);
+//
+// let scrollWidth = div.offsetWidth - div.clientWidth;
+//
+// console.log('scrollWidth:', scrollWidth);
 
-div.style.overflowY = 'scroll';
-div.style.width = '50px';
-div.style.height = '50px';
-div.style.background = 'yellow';
+// Task 2:
+const ball = document.querySelector('#ball');
+const field = document.querySelector('#field');
 
-document.body.append(div);
+ball.style.left = Math.floor(field.clientWidth / 2 - ball.offsetWidth / 2) + 'px';
+ball.style.top = Math.floor(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
 
-let scrollWidth = div.offsetWidth - div.clientWidth;
 
-console.log('scrollWidth:', scrollWidth);
 
